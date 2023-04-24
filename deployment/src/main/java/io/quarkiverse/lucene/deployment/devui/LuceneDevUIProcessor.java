b23632fb76b92889d7a883a742acbbc603f17503
melloware <mellowaredev@gmail.com>
2023-04-24 09:09:23 -0400

    Fix #104: Add DevUI Card

---
+++ deployment/src/main/java/io/quarkiverse/lucene/deployment/devui/LuceneDevUIProcessor.java
@@ -0,0 +1,33 @@
+package io.quarkus.lucene.deployment.devui;
+
+import org.apache.lucene.analysis.util.TokenizerFactory;
+
+import io.quarkus.deployment.IsDevelopment;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.devui.spi.page.CardPageBuildItem;
+import io.quarkus.devui.spi.page.Page;
+import io.quarkus.devui.spi.page.PageBuilder;
+
+/**
+ * Dev UI card for displaying important details such as the Lucene library version.
+ */
+public class LuceneDevUIProcessor {
+
+    @BuildStep(onlyIf = IsDevelopment.class)
+    void createVersion(BuildProducer<CardPageBuildItem> cardPageBuildItemBuildProducer) {
+        final CardPageBuildItem card = new CardPageBuildItem();
+
+        final PageBuilder versionPage = Page.externalPageBuilder("Lucene Version")
+                .icon("font-awesome-solid:tag")
+                .url("https://lucene.apache.org/")
+                .doNotEmbed()
+                .staticLabel(TokenizerFactory.class.getPackage().getSpecificationVersion());
+
+        card.addPage(versionPage);
+
+        card.setCustomCard("qwc-lucene-card.js");
+
+        cardPageBuildItemBuildProducer.produce(card);
+    }
+}
